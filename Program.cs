ee07b2953e77bbcece5515a2be0032eaa429e4c3
Gianna Smith <gnsmith127@gmail.com>
2021-12-03 14:53:51 -0500

    Created Personality questions and story

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MethodsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {name}. What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine($"I love {color} too! What is your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine($"Wow, {animal} is an interesting choice. What is your favorite band?");
+            string band = Console.ReadLine();
+        }
+    }
+}
