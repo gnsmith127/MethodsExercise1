9c352b2b76285449978b38f3df6e9d95b67e1605
Gianna Smith <gnsmith127@gmail.com>
2021-12-06 11:12:28 -0500

    Added Sum method with infinite params

--- Program.cs
+++ Program.cs
@@ -4,8 +4,25 @@ namespace MethodsExercise1
 {
     class Program
     {
+        public static int Sum(params int[] numbers)
+        {
+            int total = 0;
+            for (int i = 0; i < numbers.Length; i++)
+                total += numbers[i];
+            return total;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            int answer = num1 * num2;
+            return answer;
+        }
+
         static void Main(string[] args)
         {
+            int balls = Sum(1, 4, 5);
+            Console.WriteLine(balls);
+
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
 
@@ -17,6 +34,10 @@ namespace MethodsExercise1
 
             Console.WriteLine($"Wow, {animal} is an interesting choice. What is your favorite band?");
             string band = Console.ReadLine();
+
+            
         }
+
+        
     }
 }
